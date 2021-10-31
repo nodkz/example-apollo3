5092b7ee4b4a17bc690f946b4844267aef98e189
nodkz <pavel.chertorogov@gmail.com>
2021-10-31 16:53:37 +0600

    feat: use .env file which support is built-in in nextjs

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   /* config options here */
   publicRuntimeConfig: {
-    APP_GRAPHQL_URL: 'https://graphql-compose.herokuapp.com/northwind',
-    APP_GRAPHQL_WS: 'wss://graphql-compose.herokuapp.com/northwind',
+    APP_GRAPHQL_URL: process.env.APP_GRAPHQL_URL,
+    APP_GRAPHQL_WS: process.env.APP_GRAPHQL_WS,
   },
   webpack(config) {
     config.resolve.alias['app'] = path.join(__dirname, 'src');
